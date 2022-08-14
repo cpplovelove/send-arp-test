0951233db8f900b04a2cbd7024e06b0ab8cf22aa
cpplovelove <gsgslolo@naver.com>
2022-08-14 09:08:23 -0700

    check packet

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <pcap.h>
 #include "ethhdr.h"
 #include "arphdr.h"
+#include "ip.h"
 
 #include <sys/socket.h>
 #include <sys/ioctl.h>
@@ -100,6 +101,9 @@ int sendArpRequest(char* myIp, char* myMac, char* sender_ip, pcap_t* handle){
 	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");// target mac
 	packet.arp_.tip_ = htonl(Ip(sender_ip));
 
+
+
+
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
@@ -109,6 +113,69 @@ int sendArpRequest(char* myIp, char* myMac, char* sender_ip, pcap_t* handle){
 	return 1;
 }
 
+
+Mac getSenderMac(char* dev,char* senderIP){
+    struct ifreq s;
+	int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+	char* mac ;	
+	strcpy(s.ifr_name, "enp0s3");
+	ioctl(fd, SIOCGIFHWADDR, &s);
+	mac = reinterpret_cast<char *>(s.ifr_addr.sa_data);
+
+	sprintf(mac, "%02x:%02x:%02x:%02x:%02x:%02x", 
+	(unsigned char)s.ifr_hwaddr.sa_data[0],
+	(unsigned char)s.ifr_hwaddr.sa_data[1],
+	(unsigned char)s.ifr_hwaddr.sa_data[2],
+	(unsigned char)s.ifr_hwaddr.sa_data[3],
+	(unsigned char)s.ifr_hwaddr.sa_data[4],
+	(unsigned char)s.ifr_hwaddr.sa_data[5]);
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	
+
+    pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", dev, errbuf);
+	}
+
+	while (true) {
+		struct pcap_pkthdr* header;
+		const u_char* packet;
+		int res = pcap_next_ex(pcap, &header, &packet);
+		if (res == 0) continue;
+		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+			break;
+		}
+		printf("\n\n\n%u bytes captured\n", header->caplen);
+
+		
+		struct EthHdr *eth;
+		struct ArpHdr *arp_h;
+			
+		eth = (struct EthHdr *)packet;
+		packet +=sizeof(struct EthHdr);
+		arp_h = (struct ArpHdr *)packet;
+			
+	    	uint16_t eth_type = ntohs(eth -> type_);//0806  
+		uint8_t op_type = arp_h->op();
+		Mac smac = arp_h -> smac();
+		Mac dmac = arp_h -> tmac();
+		Ip sip = arp_h -> sip();
+		Ip tip = arp_h -> tip();
+		
+		printf("%04x\n", eth_type);
+		if (eth_type == 0x0806){
+			if(dmac==Mac(mac) && sip==Ip(senderIP))
+				return smac;
+			printf("%04x\n", eth_type);
+			printf("%02x\n", op_type);		
+		}	 
+		
+	}
+      pcap_close(pcap);
+
+}
 
 
 int main(int argc, char* argv[]) {
@@ -133,10 +200,10 @@ int main(int argc, char* argv[]) {
 	
 	printf("%s\n",myIp);
 	printf("%s",myMacAddress);
-	
     
 	int request_result = sendArpRequest(myIp, myMacAddress, sender_ip, handle);
 	
+	Mac src_mac = getSenderMac(dev, sender_ip);
 }
 
 
--- temp.sh
+++ temp.sh
@@ -1,2 +1,2 @@
 make
-sudo ./send-arp-test enp0s3 172.20.10.9 172.20.10.1
+sudo ./send-arp-test enp0s3 192.168.159.10 192.168.159.91
